c83abb34ed4df8d4cf3c337e347a58c574fa0c50
danya2271 <dany2271@yandex.ru>
2025-05-03 21:09:59 +0500

    Revert "Add libextendfile from oplus camera"
    This reverts commit 88b8ebfc42ecf5e3f36f98cdf18a6d40916f8e61.

--- wly-vendor.mk
+++ wly-vendor.mk
@@ -1511,7 +1511,6 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/wly/proprietary/vendor/lib64/vendor.qti.hardware.display.config-V2-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.display.config-V2-ndk_platform.so
 
 PRODUCT_PACKAGES += \
-    libextendfile \
     manifest_oplus_cameraextension_aidl.xml \
     manifest_oplus_cammidasservice_aidl.xml \
     manifest_oplus_fingerprint.xml \
